4484cb5bd2caba358aca4bd070044a1e045c4cac
TimoteDonini <142145405+TimoteDonini@users.noreply.github.com>
2023-12-04 22:43:09 +0800

    change some pages

--- deployments/docker/academy/mentors/urls.py
+++ deployments/docker/academy/mentors/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path("history",views.mentorHistory,name="mentor_history"),
     path("paypal",views.mentorPayPal,name="mentor_paypal"),
     path("mentor_changes",views.mentorChanges,name="mentor_changes"),
-    path("updateuser",views.mentorUpdateUser,name="mentor_update_user"),
-    path("userinfos",views.mentorUserInfo,name="mentor_user_infos"),
-    path("userinfos/details",views.mentorUserInfoDetails,name="mentor_user_details"),
-    path("accesscontrol",views.mentorAccessControl,name="mentor_access_control"),
-    path("manageuser",views.mentorManageUser,name="mentor_manage_user")
+    path("update_user",views.mentorUpdateUser,name="mentor_update_user"),
+    path("user_infos",views.mentorUserInfo,name="mentor_user_infos"),
+    path("user_infos/details",views.mentorUserInfoDetails,name="mentor_user_details"),
+    path("access_control",views.mentorAccessControl,name="mentor_access_control"),
+    path("manage_user",views.mentorManageUser,name="mentor_manage_user")
 ]
